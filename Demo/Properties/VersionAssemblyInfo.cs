745551f458b4b78358b1b76f34a226d24403a307
Ger Hobbelt <ger@hobbelt.com>
2021-01-25 16:46:59 +0100

    fix solutions: include SharpZipLib to ensure we can execute the demo projects in Visual Studio

---
+++ Demo/Properties/VersionAssemblyInfo.cs
@@ -0,0 +1,5 @@
+// This is a generated file.  Do not commit it to version control and do not modify it.
+using System.Reflection;
+[assembly: AssemblyVersion("2.2.195.0")]
+[assembly: AssemblyFileVersion("2.2.195.0")]
+[assembly: AssemblyInformationalVersion("2.3.0.0-datadiode-177")]
